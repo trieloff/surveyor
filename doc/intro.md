639ad0d3971666a5c342dcccdd00f3e92845d0eb
Lars Trieloff <lars.trieloff@blue-yonder.com>
2014-10-21 20:58:52 +0200

    Initial commit. Project setup

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to surveyor
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/surveyor/core_test.clj
@@ -0,0 +1,7 @@
+(ns surveyor.core-test
+  (:require [clojure.test :refer :all]
+            [surveyor.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
