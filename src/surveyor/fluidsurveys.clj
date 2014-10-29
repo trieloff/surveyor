2f58f26379e61142af0e83f702f1bcd572d785fb
Lars Trieloff <lars.trieloff@blue-yonder.com>
2014-10-29 19:59:03 +0100

    Converted JSON to Clojure map, first generator function for labels

--- src/surveyor/fluidsurveys.clj
+++ src/surveyor/fluidsurveys.clj
@@ -1,5 +1,170 @@
 (in-ns 'surveyor.core)
 
+
+{"languages"
+ [
+  {"code" "en", "name" "English", "isDefault" true}],
+ "form" [
+         {"type" "page",
+          "children" [
+                      {"showBorders" false,
+                       "unique" false,
+                       "grouped" true,
+                       "idname" "single-choice-grid",
+                       "children" [
+                                   {"type" "single", "required" false, "static" false, "label"
+                                    {"en" "My dog runs fast"}}
+                                   {"type" "single", "required" false, "static" false, "label"
+                                    {"en" "My dog barks loudly"}}],
+                       "id" "MXG7uITKA2",
+                       "alphabetize" false,
+                       "randomize" false,
+                       "randomizeLimit" false,
+                       "questionSize" 30,
+                       "uniqueError" "Responses must be unique",
+                       "title"
+                       {"en" "How important are the following outcomes for you?"},
+                       "choices" [
+                                  {"label"
+                                   {"en" "0"}}
+                                  {"label"
+                                   {"en" "1"}}
+                                  {"label"
+                                   {"en" "2"}}
+                                  {"label"
+                                   {"en" "3"}}
+                                  {"label"
+                                   {"en" "4"}}
+                                  {"label"
+                                   {"en" "5"}}
+                                  {"label"
+                                   {"en" "6"}}
+                                  {"label"
+                                   {"en" "7"}}
+                                  {"label"
+                                   {"en" "8"}}
+                                  {"label"
+                                   {"en" "9"}}
+                                  {"label"
+                                   {"en" "10"}}],
+                       "type" "question",
+                       "alternateBackground" false,
+                       "staticcol" false, "description"
+                       {"en" "Rate from 0 (not important) to 10 (most important)"}}
+                      {"showBorders" false,
+                       "unique" false,
+                       "grouped" true,
+                       "idname" "single-choice-grid",
+                       "children" [
+                                   {"type" "single", "required" false, "static" false, "label"
+                                    {"en" "Maximize the speed of my dog"}}
+                                   {"type" "single", "required" false, "static" false, "label"
+                                    {"en" "Maximize the volume of my dog's barks"}}],
+                       "id" "34Ucx2UbmL",
+                       "alphabetize" false,
+                       "randomize" false,
+                       "randomizeLimit" false,
+                       "questionSize" 30,
+                       "uniqueError"
+                       "Responses must be unique", "title"
+                       {"en" "How satisfied are you with following outcomes"},
+                       "choices" [
+                                  {"label"
+                                   {"en" "0"}}
+                                  {"label"
+                                   {"en" "1"}}
+                                  {"label"
+                                   {"en" "2"}}
+                                  {"label"
+                                   {"en" "3"}}
+                                  {"label"
+                                   {"en" "4"}}
+                                  {"label"
+                                   {"en" "5"}}
+                                  {"label"
+                                   {"en" "6"}}
+                                  {"label"
+                                   {"en" "7"}}
+                                  {"label"
+                                   {"en" "8"}}
+                                  {"label"
+                                   {"en" "9"}}
+                                  {"label"
+                                   {"en" "10"}}],
+                       "type" "question",
+                       "alternateBackground" false,
+                       "staticcol" false,
+                       "description"
+                       {"en" "Rate from 0 (not at all satisfied) to 10 (fully satisfied)"}}
+                      {"showBorders" false,
+                       "unique" false,
+                       "grouped" true,
+                       "idname" "dropdown-grid",
+                       "children" [
+                                   {"randomizeLimit" "", "alphabetize" false, "type" "dropdown", "required" false, "label"
+                                    {"en" "My dog runs fast"}, "static" false, "randomize" false}
+                                   {"randomizeLimit" "", "alphabetize" false, "type" "dropdown", "required" false, "label"
+                                    {"en" "My dog barks loudly"}, "static" false, "randomize" false}],
+                       "id" "T50nWmv9nd",
+                       "alphabetize" false,
+                       "randomize" false,
+                       "randomizeLimit" false,
+                       "questionSize" 30,
+                       "uniqueError" "Responses must be unique",
+                       "title"
+                       {"en" "How would you feel if following outcomes can be achieved."}, "choices" [
+                                                                                                      {"code" "like", "score" "", "label"
+                                                                                                       {"en" "I like it"}}
+                                                                                                      {"code" "must", "label"
+                                                                                                       {"en" "I expect it"}}
+                                                                                                      {"code" "neutral", "label"
+                                                                                                       {"en" "I don't care"}}
+                                                                                                      {"code" "tolerate", "label"
+                                                                                                       {"en" "I can tolerate it"}}
+                                                                                                      {"code" "dislike", "label"
+                                                                                                       {"en" "I dislike it"}}], "type" "question", "alternateBackground" false, "staticcol" false, "description"
+                       {"en" ""}}
+                      {"showBorders" false,
+                       "unique" false,
+                       "grouped" true,
+                       "idname" "dropdown-grid",
+                       "children" [
+                                   {"randomizeLimit" "", "alphabetize" false, "type" "dropdown", "required" false, "label"
+                                    {"en" "My dog runs fast"}, "static" false, "randomize" false}
+                                   {"randomizeLimit" "", "alphabetize" false, "type" "dropdown", "required" false, "label"
+                                    {"en" "My dog barks loudly"},
+                                    "static" false, "randomize" false}],
+                       "id" "eXlWMuDymb",
+                       "alphabetize" false,
+                       "randomize" false,
+                       "randomizeLimit" false,
+                       "questionSize" 30,
+                       "uniqueError" "Responses must be unique",
+                       "title"
+                       {"en" "How would you feel if following outcomes are prevented, this means they cannot be achieved."},
+                       "choices" [
+                                  {"code" "like", "label"
+                                   {"en" "I like it"}}
+                                  {"code" "must", "label"
+                                   {"en" "I expect it"}}
+                                  {"code" "neutral", "label"
+                                   {"en" "I don't care"}}
+                                  {"code" "tolerate", "label"
+                                   {"en" "I can tolerate it"}}
+                                  {"code" "dislike", "label"
+                                   {"en" "I dislike it"}}],
+                       "type" "question",
+                       "alternateBackground" false,
+                       "staticcol" false,
+                       "description"
+                       {"en" ""}}]}], "title"
+ {"en" "Test Survey"}}
+
+(defn create-labels
+  "Generates a sequence of labels for range responses."
+  [from to]
+  (map (fn [label] {"label" {"en" (str label)}}) (range from (inc to))))
+
 (defn create-survey
   "Creates a JSON body for new surveys based on a list of features and
   outcomes"
--- test/surveyor/core_test.clj
+++ test/surveyor/core_test.clj
@@ -1,7 +1,36 @@
 (ns surveyor.core-test
   (:require [clojure.test :refer :all]
-            [surveyor.core :refer :all]))
+            [surveyor.core :refer :all]
+            [clojure.data :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest label-generator
+  (testing "Generation of scale labels"
+    (is (= (create-labels 0 10) [
+                       {"label"
+                        {"en" "0"}}
+                       {"label"
+                        {"en" "1"}}
+                       {"label"
+                        {"en" "2"}}
+                       {"label"
+                        {"en" "3"}}
+                       {"label"
+                        {"en" "4"}}
+                       {"label"
+                        {"en" "5"}}
+                       {"label"
+                        {"en" "6"}}
+                       {"label"
+                        {"en" "7"}}
+                       {"label"
+                        {"en" "8"}}
+                       {"label"
+                        {"en" "9"}}
+                       {"label"
+                        {"en" "10"}}]))))
+
+(run-tests)
