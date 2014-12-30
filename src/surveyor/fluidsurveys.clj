28bd4b26a48a0bf783cf543992f2629ca4709a1a
Lars Trieloff <lars.trieloff@blue-yonder.com>
2014-12-30 15:39:18 +0100

    Formatted Kano Matrix

--- src/surveyor/fluidsurveys.clj
+++ src/surveyor/fluidsurveys.clj
@@ -12,11 +12,11 @@
                {"en" "I dislike it"}}])
 
 (def kano-matrix {
- [0 0] "questionable" [0 1] "attractive" [0 2] "attractive" [0 3] "attractive" [0 4] "one-dimensional"
- [1 0] "reverse" [1 1] "indifferent" [1 2] "indifferent" [1 3] "indifferent" [1 4] "must-be"
- [2 0] "reverse" [2 1] "indifferent" [2 2] "indifferent" [2 3] "indifferent" [2 4] "must-be"
- [3 0] "reverse" [3 1] "indifferent" [3 2] "indifferent" [3 3] "indifferent" [3 4] "must-be"
- [4 0] "reverse" [4 1] "reverse" [4 2] "reverse" [4 3] "reverse" [4 4] "questionable"
+ [0 0] "questionable" [0 1] "attractive"  [0 2] "attractive"  [0 3] "attractive"  [0 4] "one-dimensional"
+ [1 0] "reverse"      [1 1] "indifferent" [1 2] "indifferent" [1 3] "indifferent" [1 4] "must-be"
+ [2 0] "reverse"      [2 1] "indifferent" [2 2] "indifferent" [2 3] "indifferent" [2 4] "must-be"
+ [3 0] "reverse"      [3 1] "indifferent" [3 2] "indifferent" [3 3] "indifferent" [3 4] "must-be"
+ [4 0] "reverse"      [4 1] "reverse"     [4 2] "reverse"     [4 3] "reverse"     [4 4] "questionable"
 })
 
 ; bad, bad coder!
