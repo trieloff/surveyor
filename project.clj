685b11c9265b7c3d474de68da60d716c45e49b97
Lars Trieloff <lars.trieloff@blue-yonder.com>
2014-12-30 21:13:29 +0100

    Create command line

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.16"]
                  [clj-time "0.6.0"]
+                 [org.clojure/tools.cli "0.3.1"]
                  [cheshire "5.3.1"]]
   :main surveyor.core
   :target-path "target/%s"
--- src/surveyor/core.clj
+++ src/surveyor/core.clj
@@ -4,6 +4,7 @@
   (:require [clj-time.core :as datetime])
   (:require [clj-time.format :as timeformat])
   (:require [clojure.string :as string])
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:require [cheshire.core :refer :all])
   (:require [org.httpkit.client :as http]))
 
@@ -11,10 +12,51 @@
 (load "aggregates")
 (load "fluidsurveys")
 
+(def cli-options
+  [["-s" "--survey RELEASE" "Create a new survey for a release"
+    :validate [#(re-matches #"[A-Z]+-R-[0-9]+" %) "Must be a valid aha.io release ID"]]
+   ["-u" "--update RELEASE" "Update tags and scores for a release"
+    :validate [#(re-matches #"[A-Z]+-R-[0-9]+" %) "Must be a valid aha.io release ID"]]
+   ["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["This is my program. There are many like it, but this one is mine."
+        ""
+        "Usage:"
+        "  program-name -s SBX-R-1"
+        "  …distribute survey and wait for responses…"
+        "  program-name -s SBX-R-1"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Please refer to the manual page for more information."]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  #_(System/exit status))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    ;; Handle help and error conditions
+    (cond
+      (:help options) (exit 0 (usage summary))
+      errors (exit 1 (error-msg errors)))
+    ;; Execute program with options
+    (if (:survey options) ( println (str "Creating survey for " (:survey options) "\nPlease distribute the survey URL " (make-survey-for-release (:survey options)))))
+    (if (:update options) ( pprint/pprint (str "Updating survey for " (:update options) " " (save-results-for-release (merge-results-for-release (:update options))))))
+    ))
+
+(-main "-s" "SBX-R-3")
+
+;(-main "-u" "SBX-R-3")
 
 (defn make-survey-for-release
   [release]
@@ -61,7 +103,7 @@
 ;(println (str "Please join the " "SBX-R-3" " feature survey at " (make-survey-for-release "SBX-R-3")))
 
 ; #5 Retrieve Results
-(save-results-for-release (merge-results-for-release "SBX-R-3"))
+;(save-results-for-release (merge-results-for-release "SBX-R-3"))
 
 ; #6 Interpret Results
 ; #7 Save Results as Scores
