cbc91147cf12b2d890b23611cb30df8aa531ba7e
Lars Trieloff <lars@trieloff.net>
2016-04-04 21:57:26 +0200

    Generate NPS questions

--- src/surveyor/typeform.clj
+++ src/surveyor/typeform.clj
@@ -4,8 +4,22 @@
   (:require [surveyor.aggregates :refer :all])
   (:require [surveyor.config :refer :all]))
 
+(defn create-question-nps [name]
+  {:question (str "How likely is it that you would recommend " name " to a friend or colleague?")
+   :type "opinion_scale"
+   :labels {:left "Not likely" :right "Very likely"}
+   :ref "nps"})
+
+(defn create-question-freeform [name description]
+  {:question description
+   :type "long_text"
+   :id name})
+
 (defn create-survey
   "Creates a JSON body for new surveys based on a list of features and
   outcomes"
   [release features name]
-  (identity features))
+  {:title name
+   :webhook_submit_url (str "https://6ruu1rr486.execute-api.us-east-1.amazonaws.com/dev/" release)
+   :fields [(create-question-nps name)
+            (create-question-freeform "nps-booster" "If there was one single thing we could do to make you recommend the product, what would it be?")]})
