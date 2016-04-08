0ba6c7d98a6dc2cf0e83496418d0bf51b22d3c94
Lars Trieloff <lars@trieloff.net>
2016-04-08 11:53:18 +0200

    grouped-by-map util function

--- src/surveyor/util.clj
+++ src/surveyor/util.clj
@@ -2,5 +2,11 @@
 
 
 (defn map-a-map [f m]
+  "Equivalent to map on the values of a map"
   (into {} (for [[k v] m] [k (f v)])))
 
+(defn grouped-map-by [f m]
+  "Similar to group-by, but cleans up the resulting map by removing the grouping key f"
+  (map-a-map
+    (fn [maps] (map #(dissoc % f) maps))
+    (group-by f m)))
