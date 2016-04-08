23cb0e9cbf9f770a7ed0ad70f8b937fea74d66df
Lars Trieloff <lars@trieloff.net>
2016-04-08 14:27:27 +0200

    Term frequency aggregator

--- src/surveyor/aggregates.clj
+++ src/surveyor/aggregates.clj
@@ -1,4 +1,5 @@
-(ns surveyor.aggregates)
+(ns surveyor.aggregates
+  (:require [clojure.string :refer :all]))
 
 (defn mean [coll]
    (/ (reduce + coll) (count coll)))
@@ -8,3 +9,12 @@
 
 (defn mode [coll]
   (first (last (sort-by second (frequencies coll)))))
+
+(defn aggregate-text [coll]
+  "Return term frequencies for the words in the collection of strings"
+  (frequencies (split (clojure.string/join " " coll) #"\s")))
+
+(aggregate-text
+  '("Hallo Welt" "Hello World" "Bonjour Monde" "Hallo World"))
+
+
