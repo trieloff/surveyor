c7c3eb82dd83fdd9a7775f5079518a890a9ccb14
Lars Trieloff <lars@trieloff.net>
2016-04-08 15:03:30 +0200

    Calculate variance for NPS

--- src/surveyor/aggregates.clj
+++ src/surveyor/aggregates.clj
@@ -1,5 +1,6 @@
 (ns surveyor.aggregates
-  (:require [clojure.string :refer :all]))
+  (:require [clojure.string :refer :all])
+  (:require [clojure.math.numeric-tower :refer :all]))
 
 (defn mean [coll]
    (/ (reduce + coll) (count coll)))
@@ -18,3 +19,19 @@
   '("Hallo Welt" "Hello World" "Bonjour Monde" "Hallo World"))
 
 
+
+;(1 – NPS) ^ 2    *     #P/#T                     + (0 - NPS) ^ 2 * #N/#T                         + (-1 - NPS) ^ 2 * #D/#T
+
+(defn aggregate-nps [coll]
+  "Calculate Net Promoter Score with variance and min/max bounds."
+  (let [responents (count coll)
+        promoters  (count (filter #(> % 8) coll))
+        detractors (count (filter #(< % 7) coll))
+        nps        (quot (* 100 (- promoters detractors)) responents)
+        neutrals   (- responents promoters detractors)
+        variance   (quot (+
+                    (* (expt (- 1 nps) 2) promoters)
+                    (* (expt (- nps) 2) neutrals)
+                    (* (expt (- 1 nps) 2) detractors)) responents)]
+    {:nps nps
+     :variance  variance}))
